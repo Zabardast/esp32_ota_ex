0504c4f53355ff87b8a2c844db97068b83f42987
loup <hugo.molle@yahoo.fr>
2025-02-17 14:49:49 -0800

    🎉 add ota systems (to cleaneup)

--- esp32_ota_ex/src/main.c
+++ esp32_ota_ex/src/main.c
@@ -12,7 +12,19 @@ static const char *TCP_CLIENT = "TCP client";
 #include "keys.c"
 #include "tcp_client.c"
 
+// OTA
+#define HASH_LEN 32
 
+#include "esp_https_ota.h"  // ESP_BOOTLOADER_OFFSET
+
+#include "esp_ota_ops.h"
+
+static const char *TAG = "OTA";
+
+// "http://yourserver.com/firmware.bin" in the future use a dns or a static ip on publishing machine
+#define CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL "http://10.0.0.218:8000/firmware.bin"
+
+//
 
 void station_got_ip(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
@@ -39,12 +51,78 @@ void init_nvs()
     ESP_ERROR_CHECK(ret);
 }
 
+static void print_sha256(const uint8_t *image_hash, const char *label)
+{
+    char hash_print[HASH_LEN * 2 + 1];
+    hash_print[HASH_LEN * 2] = 0;
+    for (int i = 0; i < HASH_LEN; ++i) {
+        sprintf(&hash_print[i * 2], "%02x", image_hash[i]);
+    }
+    ESP_LOGI(TAG, "%s %s", label, hash_print);
+}
+
+void get_sha256_of_partitions()
+{
+    uint8_t sha_256[HASH_LEN] = { 0 };
+    esp_partition_t partition;
+
+    // get sha256 digest for bootloader
+    partition.address   = ESP_BOOTLOADER_OFFSET;
+    partition.size      = ESP_PARTITION_TABLE_OFFSET;
+    partition.type      = ESP_PARTITION_TYPE_APP;
+    esp_partition_get_sha256(&partition, sha_256);
+    print_sha256(sha_256, "SHA-256 for bootloader: ");
+
+    // get sha256 digest for running partition
+    esp_partition_get_sha256(esp_ota_get_running_partition(), sha_256);
+    print_sha256(sha_256, "SHA-256 for current firmware: ");
+}
+
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    return ESP_OK;
+}
+
+void simple_ota_example_task(void *pvParameter)
+{
+    ESP_LOGI(TAG, "Starting OTA example task");
+    esp_http_client_config_t config = {
+        .url = CONFIG_EXAMPLE_FIRMWARE_UPGRADE_URL,
+        // .cert_pem = (char *)server_cert_pem_start,
+        .cert_pem = NULL, // NULL because using http not https
+        .event_handler = _http_event_handler,
+        .keep_alive_enable = true,
+    };
+
+    esp_https_ota_config_t ota_config = {
+        .http_config = &config,
+    };
+
+    ESP_LOGI(TAG, "Attempting to download update from %s", config.url);
+    esp_err_t ret = esp_https_ota(&ota_config);
+    if (ret == ESP_OK) {
+        ESP_LOGI(TAG, "OTA Succeed, Rebooting...");
+        esp_restart();
+    } else {
+        ESP_LOGE(TAG, "Firmware upgrade failed: %i", ret);
+    }
+
+    // remove in the funture
+    while (1) {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
 void app_main()
 {
     // setup
     vTaskDelay(1000);
 
     init_nvs();
+
+    // check partition for OTA
+    get_sha256_of_partitions();
 
     // event loop
     esp_err_t crel_err = esp_event_loop_create_default();
@@ -92,4 +170,9 @@ void app_main()
 
     // connect to wifi AP
     esp_wifi_connect();
+
+    // set power saving to none for faster speeds (OTA speed also)
+    esp_wifi_set_ps(WIFI_PS_NONE);
+
+    xTaskCreate(&simple_ota_example_task, "ota_example_task", 8192, NULL, 5, NULL);
 }
